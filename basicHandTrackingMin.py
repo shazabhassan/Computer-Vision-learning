7336a868c23c710c555fc2cc598d3eee2e9d2d3f
Hassan SHAZAB <shazab.hassan23@my.northampton.ac.uk>
2024-05-31 01:33:28 +0100

    Continuation of the basic hand detection code,getting coordinates of the landmarks and detecting them

--- basicHandTrackingMin.py
+++ basicHandTrackingMin.py
@@ -23,8 +23,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()  # if you want to look at what the parameters that need to be put in are crl+right click on the function
 mpDraw = mp.solutions.drawing_utils
 
-
-
+#these two are setup for fps calculation
+ptime= 0
+ctime= 0
 
 
 
@@ -44,8 +45,36 @@ while True:
     if results.multi_hand_landmarks:
         # now we will do this separetely for each hand that is being detected:
         for handLms in results.multi_hand_landmarks: # we will extract all the points for each hand and draw the 20 landmarks
-            mpDraw.draw_landmarks(img,handLms)  # we will be diplaying img not results so we will draw on img
+            #the coordinates of each landmark on each hand are recorded, and we can access them in the following way
+            #each landmark has 2 attributes, id which is 0 through 19 and the coordinate which is given in a ratio
+            for id,lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                #first we will conver the ratio to pixel values
+                h,w,c = img.shape  #this gives us the full sizes of the image
+                cx , cy = int(lm.x*w) , int(lm.y*h)
+                print(id,cx , cy)
+
+                #we can even highlight individual lms now that we know thier coordinates
+                if id%2 == 0:
+                    cv2.circle(img,(cx,cy),15,(255,255,0),cv2.FILLED)                
             
+            
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)  # we will be diplaying img not results so we will draw on img
+            
+    
+    
+    #for calculating and displaying the fps
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+    #the next part will add the fps on the output window
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,255,0),3)
+    
+    
+    
+    
+    
+    
     
     
     cv2.imshow("image",img) # this will output the image detected by the webcam
